1d8c7cd8ed398c6803ebd51b97ff1c6e65e85ebf
giocaizzi <giocaizzi@gmail.com>
2022-04-03 23:00:15 +0200

    cronjob

--- tools/cronjob.sh
+++ tools/cronjob.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+echo "###############"
 echo "$(date)" 
 
 source /home/ec2-user/miniconda3/bin/activate
 conda activate cryptoenv
 
+pip install surfingcrypto --upgrade
+pip install TelegramBotNotifications --upgrade
+
 cd /home/ec2-user/surfingcrypto
-echo "### PYTHON MAIN"
+
 python main.py
 
 conda deactivate
